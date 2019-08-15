b3a14ad33cab5dbe235e7c25cee96c4cf848b070
daquan <xudaquan@ybm100.com>
2019-08-15 15:23:29 +0800

    Initial Commit

---
+++ JKDeafultViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKDeafultViewDemo
+//
+//  Created by admin on 2019/8/15.
+//  Copyright © 2019年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKDeafultViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKDeafultViewDemo
+//
+//  Created by admin on 2019/8/15.
+//  Copyright © 2019年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
