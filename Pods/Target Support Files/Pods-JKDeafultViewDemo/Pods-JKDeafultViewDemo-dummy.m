443588c6e082d2b43e6f9cbafc451247df0e96c4
daquan <xudaquan@ybm100.com>
2019-08-15 15:47:05 +0800

    init project

---
+++ Pods/Target Support Files/Pods-JKDeafultViewDemo/Pods-JKDeafultViewDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JKDeafultViewDemo : NSObject
+@end
+@implementation PodsDummy_Pods_JKDeafultViewDemo
+@end
---
+++ Pods/Target Support Files/Pods-JKDeafultViewDemoUITests/Pods-JKDeafultViewDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JKDeafultViewDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_JKDeafultViewDemoUITests
+@end
